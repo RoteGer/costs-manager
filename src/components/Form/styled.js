dc2f590438b1e390af4b7a4d51c02721a47b007e
RoteGer <rotem.ger207@gmail.com>
2023-09-09 20:56:10 +0300

    Styled the Form page

--- src/components/Form/styled.js
+++ src/components/Form/styled.js
@@ -1,77 +1,101 @@
 import styled from 'styled-components';
+import backgroundImage from './background.jpg'; // Import your background image
 
 export const FormContainer = styled.form`
   display: flex;
   flex-direction: column;
   align-items: center;
-  width:100%;
-  height: 100%;
-  background-color: #c0c0c0;
+  width: 100%;
+  max-width: 800px;
+  margin: 0 auto;
+  padding: 20px;
+  background: url(${backgroundImage}) center/cover no-repeat; /* Set the background image */
+  border-radius: 10px;
+  box-shadow: 0 0 10px rgba(0, 0, 0, 0.2);
 `;
 
 export const InputWrapper = styled.div`
   width: 100%;
-  max-width: 500px;
+  max-width: 600px;
 `;
 
 export const FormInput = styled.input`
   padding: 15px 20px;
-  margin: 20px 10px;
-  font-size: 18px;
-  border-radius: 10px;
-  border: none;
-  background-color: white;
-  box-shadow: 0 0 10px gray;
-  width: 400px;
+  margin: 10px 0;
+  font-size: 16px;
+  font-weight: bold; /* Add bold text */
+  border-radius: 8px;
+  border: 1px solid #ccc;
+  background-color: #fff;
+  width: 100%;
+  transition: border-color 0.3s ease-in-out;
+  &:focus {
+    outline: none;
+    border-color: #2aaa8a;
+  }
 `;
 
 export const FormSelect = styled.select`
   padding: 15px 20px;
-  margin: 20px 10px;
-  font-size: 18px;
-  border-radius: 10px;
-  border: none;
-  background-color: white;
-  box-shadow: 0 0 10px gray;
-  width: 440px;
+  margin: 10px 0;
+  font-size: 16px;
+  font-weight: bold; /* Add bold text */
+  border-radius: 8px;
+  border: 1px solid #ccc;
+  background-color: #fff;
+  width: 100%;
+  transition: border-color 0.3s ease-in-out;
+  &:focus {
+    outline: none;
+    border-color: #2aaa8a;
+  }
 `;
 
 export const FormTextArea = styled.textarea`
   padding: 15px 20px;
-  margin: 20px 10px;
-  font-size: 18px;
-  border-radius: 10px;
-  border: none;
-  background-color: white;
-  box-shadow: 0 0 10px gray;
-  width: 400px;
-  height: 50px;
+  margin: 10px 0;
+  font-size: 16px;
+  font-weight: bold; /* Add bold text */
+  border-radius: 8px;
+  border: 1px solid #ccc;
+  background-color: #fff;
+  width: 100%;
+  min-height: 100px;
+  transition: border-color 0.3s ease-in-out;
+  &:focus {
+    outline: none;
+    border-color: #2aaa8a;
+  }
 `;
 
 export const FormButton = styled.button`
   padding: 15px 40px;
-  margin: 20px 0px 20px;
+  margin: 20px 0;
   font-size: 18px;
-  border-radius: 10px;
-  background-color: #00a36c;
-  color: white;
-  width: 40%;
+  font-weight: bold; /* Add bold text */
+  border-radius: 8px;
+  background-color: #2aaa8a;
+  color: #fff;
+  width: 50%;
   cursor: pointer;
+  transition: background-color 0.3s ease-in-out;
   &:hover {
-    background-color: #2aaa8a;
+    background-color: #1a7a60;
   }
   &:focus {
     outline: none;
-    background-color: #478778;
   }
 `;
 
 export const FormLabel = styled.label`
-  font-size: 20px;
-  margin: 10px 10px;
-  color: black;
+  font-size: 18px;
+  margin: 10px 0;
+  color: #333;
+  font-weight: bold; /* Add bold text */
 `;
+
 export const InputContainer = styled.div`
   display: flex;
-  align-items: center;
+  flex-direction: column;
+  align-items: flex-start;
 `;
